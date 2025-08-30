97c639b6276cb92553791c0ee77105b0f7c54b55
ChrisGamer503 <106628105+ChrisGamer503@users.noreply.github.com>
2025-08-30 14:31:15 -0600

    GUIAS

---
+++ Semana 8/spring-database-springdata/src/main/java/sv/edu/udb/repository/PostCommentRepository.java
@@ -0,0 +1,35 @@
+package sv.edu.udb.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import sv.edu.udb.repository.domain.PostComment;
+
+import java.util.List;
+
+@Repository
+public class PostCommentRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<PostComment> findAll() {
+        final String QUERY = "SELECT pc FROM PostComment pc";
+        return entityManager.createQuery(QUERY, PostComment.class).getResultList();
+    }
+
+    public PostComment findById(Long id) {
+        return entityManager.find(PostComment.class, id);
+    }
+
+    @Transactional
+    public void save(PostComment postComment) {
+        entityManager.persist(postComment);
+    }
+
+    @Transactional
+    public void delete(PostComment postComment) {
+        entityManager.remove(postComment);
+    }
+}
---
+++ Semana9/spring-api-rest/src/test/java/sv/edu/udb/controller/PostControllerTest.java
@@ -0,0 +1,67 @@
+package sv.edu.udb.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import sv.edu.udb.controller.response.PostResponse;
+import sv.edu.udb.repository.domain.Post;
+import sv.edu.udb.service.PostService;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+class PostControllerTest {
+
+    private MockMvc mockMvc;
+    private ObjectMapper objectMapper;
+
+    @Mock
+    private PostService postService;
+
+    @InjectMocks
+    private PostController postController;
+
+    private Post post;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(postController).build();
+        this.objectMapper = new ObjectMapper();
+
+        this.post = Post.builder()
+                .id(1L)
+                .title("testing post")
+                .postDate(LocalDate.of(2024, 9, 28))
+                .build();
+    }
+
+    @Test
+    @DisplayName("Get all posts")
+    void findAllPost_when_performGetRequest() throws Exception {
+        final List<PostResponse> expectedList = List.of(
+                PostResponse.builder()
+                        .title(post.getTitle())
+                        .postDate(post.getPostDate())
+                        .build()
+        );
+
+        when(postService.findAll()).thenReturn(expectedList);
+
+        this.mockMvc.perform(get("/posts"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(objectMapper.writeValueAsString(expectedList)));
+    }
+}
